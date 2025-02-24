0b0c7628fc4ec1fa22fa8db19668d4f1ceb55aa7
Alfredo Palmieri Paso <app@cloudworksglobal.tech>
2025-02-24 00:42:47 -0300

    last changes

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-blue-600 p-4 mt-16 text-white text-center">
-      <p>© 2025 Cloudworks. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
